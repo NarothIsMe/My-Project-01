9455753af44d6033cf40f078fdfc1b56aee8dc0b
ChhaySovanDara <chhaysovandara5975@gmail.com>
2024-07-25 09:04:03 +0700

    School

--- add.php
+++ add.php
@@ -1,26 +1,37 @@
 <?php
-    include_once('db.php');
-    $output = array('error' => false);
-    $database = new connDB();
-    $db = $database->open();
-    try{
-        //use of prepared statement to prevent sql injection
-        $stmt = $db->prepare("INSERT INTO members (firstname, lastname, address) VALUES (:firstname, :lastname, :address)");
-        //if-else statement executing prepared statement
-        if ($stmt->execute(array(':firstname' => $_POST['firstname'] , ':lastname' => $_POST['lastname'] , ':address' => $_POST['address'])) ){
-            $output['message'] = 'Member added successfully';
-        }
-        else{
-            $output['error'] = true;
-            $output['message'] = 'Something went wrong. Cannot add member';
-        } 
-    }
-    catch(PDOException $e){
+// Include the database connection class
+include_once('db.php');
+
+// Initialize an array to store output messages
+$output = array('error' => false);
+
+// Create an instance of connDB to establish a database connection
+$database = new connDB();
+$db = $database->open(); // Open the database connection
+
+try {
+    // Use a prepared statement to prevent SQL injection
+    $stmt = $db->prepare("INSERT INTO members (firstname, lastname, address) VALUES (:firstname, :lastname, :address)");
+
+    // If-else statement executing the prepared statement
+    if ($stmt->execute(array(':firstname' => $_POST['firstname'] , ':lastname' => $_POST['lastname'] , ':address' => $_POST['address']))) {
+        // Set success message if the member is added successfully
+        $output['message'] = 'Member added successfully';
+    } else {
+        // Set error message if something went wrong during member addition
         $output['error'] = true;
-        $output['message'] = $e->getMessage();
-    }
-    //close connection
-    $database->close();
- 
-    echo json_encode($output);
+        $output['message'] = 'Something went wrong. Cannot add member';
+    } 
+            
+} catch(PDOException $e) {
+    // Catch any exceptions and set error message
+    $output['error'] = true;
+    $output['message'] = $e->getMessage();
+}
+
+// Close the database connection
+$database->close();
+
+// Encode the output array to JSON format and output it
+echo json_encode($output);
 ?>
--- fetch.php
+++ fetch.php
@@ -1,29 +1,41 @@
 <?php
-    include_once('db.php');
-    $database = new connDB();
-    $db = $database->open();
- 
-    try{    
-        $sql = 'SELECT * FROM members';
-        foreach ($db->query($sql) as $row) {
-            ?>
-            <tr>
-                <td><?php echo $row['id']; ?></td>
-                <td><?php echo $row['firstname']; ?></td>
-                <td><?php echo $row['lastname']; ?></td>
-                <td><?php echo $row['address']; ?></td>
-                <td>
-                    <button class="btn btn-success btn-sm edit" data-id="<?php echo $row['id']; ?>"><span class="glyphicon glyphicon-edit"></span> Edit</button>
-                    <button class="btn btn-danger btn-sm delete" data-id="<?php echo $row['id']; ?>"><span class="glyphicon glyphicon-trash"></span> Delete</button>
-                </td>
-            </tr>
-            <?php 
-        }
+// Include the database connection class
+include_once('db.php');
+
+// Create an instance of connDB to establish a database connection
+$database = new connDB();
+$db = $database->open(); // Open the database connection
+
+try {
+    // SQL query to select all columns from the members table
+    $sql = 'SELECT * FROM members';
+    // Execute the SQL query and iterate over the result set
+    foreach ($db->query($sql) as $row) {
+        // Output HTML table row with data from each row in the result set
+?>
+        <tr>
+            <td><?php echo $row['id']; ?></td>
+            <td><?php echo $row['firstname']; ?></td>
+            <td><?php echo $row['lastname']; ?></td>
+            <td><?php echo $row['address']; ?></td>
+            <td>
+                <!-- Edit button with data-id attribute containing the member's ID -->
+                <button class="btn btn-success btn-sm edit" data-id="<?php echo $row['id']; ?>">
+                    <span class="glyphicon glyphicon-edit"></span> Edit
+                </button>
+                <!-- Delete button with data-id attribute containing the member's ID -->
+                <button class="btn btn-danger btn-sm delete" data-id="<?php echo $row['id']; ?>">
+                    <span class="glyphicon glyphicon-trash"></span> Delete
+                </button>
+            </td>
+        </tr>
+<?php 
     }
-    catch(PDOException $e){
-        echo "There is some problem in connection: " . $e->getMessage();
-    }
- 
-    //close connection
-    $database->close();
+} catch(PDOException $e) {
+    // Catch any exceptions and display an error message
+    echo "There is some problem in connection: " . $e->getMessage();
+}
+
+// Close the database connection
+$database->close();
 ?>
